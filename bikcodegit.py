d7f3b8aefdcee2fc5e04e72f9e516a7be339496d
askcd2020 <ashokcd@hotmail.com>
2020-08-02 14:34:46 -0400

    Initial checkin.

---
+++ bikcodegit.py
@@ -0,0 +1,11 @@
+print("I am Python programmer.")
+x = 5
+y = 2
+z = 3
+print("x + y =", x + y) # operator +
+print("x - y =", x - y) # operator -
+print("x / z =", x / z) # operator / (result is float)
+print("x % y =", x % y) # operator % (modulo)
+print("x // y =", x // y) # operator // (integer division)
+print("y * z =", y * z) # operator * (multiply)
+print("y ** z =", y ** z) # operator ** (exponent)
